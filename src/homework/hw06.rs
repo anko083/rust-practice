54d32c71f6b4a215fd7ff73d414c21ad0cd66ce9
Anna <anna.durybaba@e-u.edu.ua>
2025-04-09 02:55:52 +0300

    add taskn

--- src/homework/hw06.rs
+++ src/homework/hw06.rs
@@ -1,21 +1,20 @@
 fn draw_tree(triangles: usize) {
-    let height = 5;  // Висота кожного трикутника
-    let width = height * 2 - 1; // Ширина найбільшого трикутника
-
-    // Малюємо кожний трикутник
+    let height = 5;  
+    let width = height * 2 - 1; 
+    
     (1..=triangles).for_each(|i| {
         (0..height).for_each(|j| {
-            let stars = 2 * j + 1; // Кількість зірочок на поточному рівні
-            let spaces = (width - stars) / 2; // Кількість пробілів зліва від зірочок
+            let stars = 2 * j + 1; 
+            let spaces = (width - stars) / 2; 
 
-                    // Виводимо рядок з пробілами та зірочками
+                    
                     (0..spaces).for_each(|_| print!(" "));
                     (0..stars).for_each(|_| print!("*"));
-                    println!(); // Перехід на наступний рядок
+                    println!(); 
                 });
             });
         
-            // Малюємо стовпчик ялинки
+           
             (0..2).for_each(|_| {
                 (0..(width / 3)).for_each(|_| print!(" "));
                 (0..(width / 3)).for_each(|_| print!("*"));
@@ -24,6 +23,6 @@ fn draw_tree(triangles: usize) {
         }
         
         fn main() {
-            let triangles = 3; // Кількість трикутників для ялинки
+            let triangles = 3; 
             draw_tree(triangles);
         }
